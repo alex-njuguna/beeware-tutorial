57f8c0de45b39268b4f5c933cbce1ffdf18f5690
alex-njuguna <njugunakinuthia013@gmail.com>
2024-07-05 01:18:01 +0300

    update package

--- helloworld/src/helloworld/app.py
+++ helloworld/src/helloworld/app.py
@@ -7,6 +7,11 @@ from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
 
+def greeting(name):
+    if name:
+        return f"Hello, {name}"
+    return "Hello, stranger"
+
 class HelloWorld(toga.App):
     def startup(self):
 
@@ -36,7 +41,10 @@ class HelloWorld(toga.App):
         self.main_window.show()
 
     def say_hello(self, widget):
-        print(f"Hello, {self.name_input.value}")
+        self.main_window.info_dialog(
+            greeting(self.name_input.value),
+            "Hi there!"
+        )
 
 
 def main():
